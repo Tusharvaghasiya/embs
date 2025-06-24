5046c2c724043b110bfe6497db91ac691ad595b1
Tushar Vaghasiya <17301004tushar@viva-technology.org>
2025-06-24 21:56:30 +0530

    Remove unused imports across DTOs, exception handler, and UserController

--- src/main/java/org/project/sembs/evntmngmtbksytm/dto/RoleUpdateRequest.java
+++ src/main/java/org/project/sembs/evntmngmtbksytm/dto/RoleUpdateRequest.java
@@ -1,8 +1,6 @@
 package org.project.sembs.evntmngmtbksytm.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
 import org.project.sembs.evntmngmtbksytm.model.Role;
 import org.project.sembs.evntmngmtbksytm.validation.EnumValidator;
